2859e6d6121effba9524d6b275e2b7000ab45fca
Leandro Silva Sampaio <168847269+LeandroSampaio7@users.noreply.github.com>
2024-05-03 18:07:42 -0300

    upload site pessoal

---
+++ webpage/js/main.js
@@ -0,0 +1,61 @@
+window.addEventListener("scroll", function(){
+  let header = document.querySelector('#header')
+  header.classList.toggle('scrol-menu', window.scrollY > 100)
+})
+
+const state = {};
+const carouselList = document.querySelector('.carousel__list');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const elems = Array.from(carouselItems);
+
+carouselList.addEventListener('click', function (event) {
+  var newActive = event.target;
+  var isItem = newActive.closest('.carousel__item');
+
+  if (!isItem || newActive.classList.contains('carousel__item_active')) {
+    return;
+  };
+  
+  update(newActive);
+});
+
+const update = function(newActive) {
+  const newActivePos = newActive.dataset.pos;
+
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const prev = elems.find((elem) => elem.dataset.pos == -1);
+  const next = elems.find((elem) => elem.dataset.pos == 1);
+  const first = elems.find((elem) => elem.dataset.pos == -2);
+  const last = elems.find((elem) => elem.dataset.pos == 2);
+  
+  current.classList.remove('carousel__item_active');
+  
+  [current, prev, next, first, last].forEach(item => {
+    var itemPos = item.dataset.pos;
+
+    item.dataset.pos = getPos(itemPos, newActivePos)
+  });
+};
+
+const getPos = function (current, active) {
+  const diff = current - active;
+
+  if (Math.abs(current - active) > 2) {
+    return -current
+  }
+
+  return diff;
+}
+
+function menuShow(){
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "./img/iconMenuMobile.png";
+  }else{
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "./img/iconMenuMobileClose.png";
+  }
+}
+
+
